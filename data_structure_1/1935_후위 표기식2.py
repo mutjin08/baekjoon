52d00bdb8ace91cb443ed81a5cac9c81eac62300
mutjin08 <mutjin08@gmail.com>
2026-03-01 12:56:42 +0900

    26-03-01 improved

--- data_structure_1/1935_후위 표기식2.py
+++ data_structure_1/1935_후위 표기식2.py
@@ -1,42 +1,27 @@
-def calculate(b, a, operator):
-
-    if operator == "+":
-        return a+b
-    
-    elif operator == "-":
-        return a-b
-
-    elif operator == "*":
-        return a*b
-
-    elif operator == "/":
-        return a/b
-
+def calculate(e, b, a):
+    if e == "+":
+        return a + b
+    elif e == "-":
+        return a - b
+    elif e == "*":
+        return a * b
+    elif e == "/":
+        return a / b
     else:
         return -1
-
-
 
 n = int(input())
 exp = input()
 
-#replace
-
 alp_to_num = {}
-pin = 0
-for _ in range(n):
-    alp_to_num[exp[pin]] = float(input())
-
-    pin+=1
-    while pin<len(exp) and not exp[pin].isalpha():
-        pin+=1
-
-
 s = []
 for e in exp:
     if e.isalpha():
+        if e not in alp_to_num:
+            alp_to_num[e] = float(input())
         s.append(alp_to_num[e])
     else:
-        s.append(calculate(s.pop(), s.pop(), e))
+        s.append(calculate(e, s.pop(), s.pop()))
+    
 
 print("{0:.2f}".format(s[0]))
